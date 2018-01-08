74f7b08f46d046058059a444e368f24c2c1e92a5
Thomas van Aart <thomasvanaart@casema.nl>
2018-01-08 10:44:39 +0100

    Added a lot of progress. And fix a Bootstrap bug that prevents from building

--- src/app/shared/components/searchbar/searchbar.component.ts
+++ src/app/shared/components/searchbar/searchbar.component.ts
@@ -3,7 +3,8 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
 
 const countries = ['Afghanistan', 'Albania', 'Algeria', 'America', 'Andorra', 'Angola', 'Antigua', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bissau', 'Bolivia', 'Bosnia', 'Botswana', 'Brazil', 'British', 'Brunei', 'Bulgaria', 'Burkina', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 'Costa Rica', 'country debt', 'Croatia', 'Cuba', 'Cyprus', 'Czech', 'Denmark', 'Djibouti', 'Dominica', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Emirate', 'England', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Great Britain', 'Greece', 'Grenada', 'Grenadines', 'Guatemala', 'Guinea', 'Guyana', 'Haiti', 'Herzegovina', 'Honduras', 'Hungary', 'Iceland', 'in usa', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Samoa', 'San Marino', 'Sao Tome', 'Saudi Arabia', 'scotland', 'scottish', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon', 'Somalia', 'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'St. Kitts', 'St. Lucia', 'St Kitts', 'St Lucia', 'Saint Kitts', 'Santa Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Tobago', 'Togo', 'Tonga', 'Trinidad', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Kingdom', 'United States', 'Uruguay', 'USA', 'Uzbekistan', 'Vanuatu', 'Vatican', 'Venezuela', 'Vietnam', 'wales', 'welsh', 'Yemen', 'Zambia', 'Zimbabwe', ];
 
@@ -18,19 +19,24 @@ export class SearchbarComponent implements OnInit {
   public searchForm: FormGroup;
   public model: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.model = localStorage.getItem('values');
+
     this.searchForm = new FormGroup({
       place: new FormControl(null, []),
       from: new FormControl(null, []),
       until: new FormControl(null, []),
-      guests: new FormControl(0, [Validators.required])
+      guests: new FormControl(null, [Validators.required])
     });
   }
 
   onSubmit() {
-    alert(JSON.stringify(this.searchForm.value));
+    // alert(JSON.stringify(this.searchForm.value));
+    localStorage.setItem('values', JSON.stringify(this.searchForm.value))
+    this.router.navigate(['results']);
   }
 
   search = (text$: Observable<string>) =>
